b83ee5f279c9de9f5396c6c94590fd7e53312e85
wonsc12 <jangkipong12@gmail.com>
2024-05-27 12:01:23 +0900

    my nav my arctcle 추가

--- src/App.js
+++ src/App.js
@@ -21,11 +21,38 @@ class Myheader extends Component {
     )
   }
 }
+class Mynav extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="">HTML</a></li>
+          <li><a href="">CSS</a></li>
+          <li><a href="">Javascript</a></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+class Myarticle extends Component {
+  render() {
+    return (
+      <section>
+        <article>
+          <h2>HTML</h2>
+          <p>Hypertext markup Language</p>
+        </article>
+      </section>
+    )
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Myheader></Myheader>
+        <Myheader/>
+        <Mynav/>
+        <Myarticle/>
       </div>
     )
   }
